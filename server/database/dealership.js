cf37cee4a012088452487992ff3bb1df586f4166
karthik-bha <143622670+karthik-bha@users.noreply.github.com>
2024-04-09 20:26:25 +0530

    Update dealership.js

--- server/database/dealership.js
+++ server/database/dealership.js
@@ -1,43 +1,45 @@
-const mongoose = require('mongoose');
+'use strict';
 
-const Schema = mongoose.Schema;
+var mongoose = require('mongoose');
 
-const dealerships = new Schema({
-	id: {
-    type: Number,
-    required: true,
-	},
-	city: {
-    type: String,
-    required: true
-  },
-  state: {
-    type: String,
-    required: true
-  },
-  address: {
-    type: String,
-    required: true
-  },
-  zip: {
-    type: String,
-    required: true
-  },
-  lat: {
-    type: String,
-    required: true
-  },
-  long: {
-    type: String,
-    required: true
-  },
-  short_name: {
-    type: String,
-  },
-  full_name: {
-    type: String,
-    required: true
-  }
+var Schema = mongoose.Schema;
+
+var dealerships = new Schema({
+    id: {
+        type: Number,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    state: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    zip: {
+        type: String,
+        required: true
+    },
+    lat: {
+        type: String,
+        required: true
+    },
+    long: {
+        type: String,
+        required: true
+    },
+    short_name: {
+        type: String
+    },
+    full_name: {
+        type: String,
+        required: true
+    }
 });
 
 module.exports = mongoose.model('dealerships', dealerships);
